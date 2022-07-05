31293dd079ccd647da2300bcf9110a9fed1b4d86
Szabolcs11 <67874109+Szabolcs11@users.noreply.github.com>
2022-07-06 00:04:16 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,99 @@
+let elipse1 = document.getElementById("elipse-1")
+let elipse2 = document.getElementById("elipse-2")
+let elipse3 = document.getElementById("elipse-3")
+let elipse4 = document.getElementById("elipse-4")
+let elipse5 = document.getElementById("elipse-5")
+
+let images2 = [
+    ["url('./aseets/image 14.jpg')", elipse1],
+    ["url('./aseets/4541cc99083f618a22b772228f8a9698@2x 1.jpg')", elipse2],
+    ["url('./aseets/image 14.jpg')", elipse3],
+    ["url('./aseets/victoria-shes-UC0HZdUitWY-unsplash copy@2x 1.jpg')", elipse4],
+    ["url('./aseets/80457d7e9d392ee84954d90d13d15d69@2x 1.png')", elipse5]
+]
+let i = 0;
+
+setTimeout(() => {
+    changeimage()
+}, 1000)
+
+function changeimage() {
+    if (i==images2.length-1) {
+        i=0;
+    }
+    else {
+        i++;
+    }
+    changed()
+
+
+    setTimeout(() => {
+        changeimage()
+    }, 2000)
+}
+
+function changed() {
+    document.getElementsByClassName("navigation-active")[0].classList.remove("navigation-active")
+    images2[i][1].classList.add("navigation-active")
+    document.getElementById("slider-container").style.backgroundImage = images2[i][0]
+}
+
+function NavigateRight() {
+    if (i==images2.length-1) {
+        i=0;
+    }
+    else {
+        i++;
+    }
+    changed()
+}
+
+function NavigateLeft() {
+    if (i==0) {
+        i=4;
+    }
+    else {
+        i=i-1
+    }
+    changed()
+}
+
+function GoTo(value, element) {
+    i = value
+    changed()
+}
+
+
+
+// Card Navigation \\
+let j = 0
+function CardLeft() {
+    if (j==0) {
+        j=2;
+    }
+    else {
+        j--;
+    }
+    CardChanged()
+}
+
+function CardRight() {
+    if (j==2) {
+        j=0;
+    }
+    else {
+        j++;
+    }
+    CardChanged()
+}
+
+function CardChanged() {
+    console.log(j)
+}
+
+
+
+
 function HamburgerMenuClick() {
     let menu = document.getElementById("hamburgermenu")
     if (menu.classList.contains("toggle")) {
